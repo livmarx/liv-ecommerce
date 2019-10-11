e7607f2c8eb5428d26483bbff58865577026f16b
Liv Marx <oehrenmarks@gmail.com>
2019-10-11 13:09:14 -0400

    add styling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import { auth } from './firebase/firebase.utils';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFormAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFormAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log('user:', user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFormAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
